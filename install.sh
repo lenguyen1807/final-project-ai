224f14e833fcf03e4d5a7faa8c3234959923ef84
Nguyen <lenguyen18072003@gmail.com>
2025-10-27 09:06:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,13 @@
 set -x #echo on
 
 # Install nvidia
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-rm -r cuda-keyring_1.1-1_all.deb
+curl -fsSL https://get.gpuup.sh | bash
 
 # Update system
 sudo add-apt-repository ppa:quentiumyt/nvtop -y
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt-get install build-essential cuda-toolkit nodejs npm nvidia-gds btop nvtop -y
+sudo apt-get install build-essential nodejs npm nvidia-gds btop nvtop unzip -y
 
 # Install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
